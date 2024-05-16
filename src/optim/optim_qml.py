5e76e1444b741b2c294f24e4e7dab3915c942395
Kerem Bükrü <kerembuekrue@gmail.com>
2024-05-16 17:11:53 +0200

    split optim.py into three separate files, qml, tf, torch

--- src/optim/optim_qml.py
+++ src/optim/optim_qml.py
@@ -1,61 +1,75 @@
 from time import time
 import pennylane as qml
-from pennylane import qchem
 import pennylane.numpy as np
 import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
 
 
-class Optimizer:
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        result = func(*args, **kwargs)
+        end_time = time()
+        print()
+        print(f"Function '{func.__name__}' total computation time: {end_time - start_time:.7f} seconds")
+        print()
+        return result
+
+    return wrapper
+
+
+def plot_loss(loss_hist):
+    plt.figure(1)
+    plt.plot(loss_hist, "k", linewidth=2)
+    plt.ylabel("Cost function")
+    plt.xlabel("Optimization steps")
+    plt.show()
+
+
+class Optimizer(ABC):
     def __init__(self, eta, tol, maxiter, nqubits):
         self.eta = eta
         self.tol = tol
         self.maxiter = maxiter
         self.nqubits = nqubits
 
-    def solve(self, weights):
-        pass
-
+    @timing_decorator
     def optimize(self, func):
+        # Initial weights for strongly entangling layers
+        w = 1.0 * np.random.randn(self.nqubits, requires_grad=True)
 
-        q_delta = 0.001 * np.pi  # Initial spread of random quantum weights
-
-        # initial weights for strongly entangling layers
-        w = q_delta * np.random.randn(self.nqubits, requires_grad=True)
-
-        # Gradient Descent Optimization Algorithm
-        opt = qml.GradientDescentOptimizer(self.eta)
+        # Get the optimizer from the child class
+        opt = self.get_optimizer()
 
         # Optimization loop
-        cost_history = []
-        t0 = time()
+        cost_vals = []
         for it in range(self.maxiter):
             ta = time()
             w, cost_val = opt.step_and_cost(func, w)
             print("Step {:3d}    obj = {:9.7f}    time = {:9.7f} sec".format(it, cost_val, time() - ta))
-            if np.abs(cost_val) < 1e-4:
+            if np.abs(cost_val) < self.tol:
                 break
-            cost_history.append(cost_val)
-        print("\n Total Optimization Time: ", time() - t0, " sec")
+            cost_vals.append(cost_val)
 
-        return w
+        return w, cost_vals
 
-    def plot_loss(self, loss_hist):
-        plt.figure(1)
-        plt.plot(loss_hist, "k", linewidth=2)
-        plt.ylabel("Cost function")
-        plt.xlabel("Optimization steps")
+    @abstractmethod
+    def get_optimizer(self):
+        pass
 
 
 class GradientDescent_pennylane(Optimizer):
     def __init__(self, eta, tol, maxiter, nqubits):
         super().__init__(eta, tol, maxiter, nqubits)
 
+    def get_optimizer(self):
+        return qml.GradientDescentOptimizer(self.eta)
+
     def vqc(self, weights):
         raise NotImplementedError("Not implemented yet.")
 
 
 if __name__ == "__main__":
-
     n = 6
 
     dev = qml.device("default.qubit", wires=n)
@@ -79,6 +93,5 @@ if __name__ == "__main__":
 
     solver = GradientDescent_pennylane(eta=0.8, tol=0.01, maxiter=10, nqubits=n)
 
-    wopt = solver.optimize(cost, )
-
-    print("wopt =", wopt)
+    wopt, cost_history = solver.optimize(cost)
+    plot_loss(cost_history)
